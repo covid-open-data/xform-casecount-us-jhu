0c10d7bb5b93d743ffb1d858afef621075d9b8ee
Ryan Hafen <rhafen@gmail.com>
2020-05-06 16:29:05 -0700

    Add US data transformers

--- src/transformer.R
+++ src/transformer.R
@@ -18,15 +18,15 @@ fix_date <- function(dt) {
 
 # pivot from wide to long, fix date and names
 process <- function(x, name) {
+  rnm <- function(x)
+    gsub("\\/", "_", tolower(x))
+
   x %>%
     tidyr::pivot_longer(
       cols = ends_with("20"),
       names_to = "date",
       values_to = name) %>%
-    dplyr::rename(
-      province_state = "Province/State",
-      country_region = "Country/Region") %>%
-    dplyr::rename_all(tolower) %>%
+    dplyr::rename_all(rnm) %>%
     dplyr::mutate(date = fix_date(date))
 }
 
@@ -34,7 +34,7 @@ dc <- process(dc, "n_case")
 dd <- process(dd, "n_death")
 
 # join cases and deaths
-d <- left_join(dc, select(dd, -lat, -long),
+d <- dplyr::left_join(dc, select(dd, -lat, -long),
   by = c("province_state", "country_region", "date"))
 
 # they break things down to admin1 level in some cases - want to roll these up
@@ -55,12 +55,57 @@ admin0 <- d %>%
 
 # TODO: need to use "datautils" functions here to get ISO2 codes
 
-readr::write_csv(admin0, "admin0.csv")
-
 # TODO: need to use "datautils" here to roll up to continent, WHO region
 
 global <- admin0 %>%
   dplyr::group_by(date) %>%
   dplyr::summarise(cases = sum(cases), deaths = sum(deaths))
 
-readr::write_csv(admin0, "global.csv")
+readr::write_csv(admin0, "admin0.csv")
+readr::write_csv(global, "global.csv")
+
+# now pull US state and county-level JHU data
+
+us_urls <- list(
+  cases = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv",
+  deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+)
+
+usdc <- suppressMessages(readr::read_csv(us_urls$cases))
+usdd <- suppressMessages(readr::read_csv(us_urls$deaths))
+
+us_process <- function(x, name) {
+  rnm <- function(x)
+    gsub("\\/", "_", tolower(x))
+
+  x %>%
+    dplyr::mutate(
+      fips_county = sprintf("%05d", FIPS),
+      fips_state = substr(fips_county, 1, 2)
+    ) %>%
+    # 80 and 90 are "out of state" or "unassigned"
+    # 88 and 99 are cruise ships
+    # 00 have NA FIPS - ignore all these for now
+    dplyr::filter(!fips_state %in% c("80", "90", "00", "88", "99")) %>%
+    dplyr::select(fips_county, fips_state, tidyselect::ends_with("20")) %>%
+    tidyr::pivot_longer(
+      cols = ends_with("20"),
+      names_to = "date",
+      values_to = name) %>%
+    dplyr::rename_all(rnm) %>%
+    dplyr::mutate(date = fix_date(date))
+}
+
+usdc <- us_process(usdc, "cases")
+usdd <- us_process(usdd, "deaths")
+
+# join cases and deaths
+usd <- dplyr::left_join(usdc, usdd,
+  by = c("fips_county", "fips_state", "date"))
+
+usd_state <- usd %>%
+  dplyr::group_by(fips_state, date) %>%
+  dplyr::summarise(cases = sum(cases), deaths = sum(deaths))
+
+readr::write_csv(usd, "admin2_US.csv")
+readr::write_csv(usd_state, "admin1_US.csv")
