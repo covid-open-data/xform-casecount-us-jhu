628016727c8c03348324f462ccab6c4e86d67697
Patrick <pcstout@gmail.com>
2020-06-25 09:57:43 -0600

    Update .github

--- .github/actions/git-push/install.sh
+++ .github/actions/git-push/install.sh
@@ -3,6 +3,5 @@
 # Install base container dependencies.
 ###############################################################################
 source "${GITHUB_WORKSPACE}/.github/scripts/shutils.sh"
-installAptPackages apt-utils git curl
 git --version
 exit $?
--- .github/actions/run-xform/install.sh
+++ .github/actions/run-xform/install.sh
@@ -3,10 +3,5 @@
 # Install base container dependencies.
 ###############################################################################
 source "${GITHUB_WORKSPACE}/.github/scripts/shutils.sh"
-installAptPackages apt-utils software-properties-common dirmngr apt-transport-https curl
-add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran36/' --yes
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "E298A3A825C0D65DFD57CBB651716619E084DAB9"
-apt-get update
-installAptPackages r-base r-base-core r-base-dev r-recommended
 R --version
 exit $?
